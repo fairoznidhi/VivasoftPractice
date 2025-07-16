444c3defb60e380d89d29bb08d692ee3344d4e00
fairoznidhi <fairoz1697@gmail.com>
2025-07-16 18:51:45 +0600

    16/7/25

--- programming-hero/todo-module-3/src/App.tsx
+++ programming-hero/todo-module-3/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "./components/ui/button";
+import Todo from "./pages/Todo";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Todo/>
     </div>
   );
 }
