ec3127906eb6d0f9ed84a8ca53848a78b666e341
fairoznidhi <fairoz1697@gmail.com>
2025-07-17 17:30:07 +0600

    17/7/25

--- programming-hero/todo-module-3/src/components/todo/TodoContainer.tsx
+++ programming-hero/todo-module-3/src/components/todo/TodoContainer.tsx
@@ -1,14 +1,29 @@
+import { useGetTodosQuery } from "../../redux/api/api";
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+
 const TodoContainer = () => {
+  //From local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  //From server
+  const {data:todos,isError,isLoading}=useGetTodosQuery(undefined)
   return (
     <div>
-      <div>
-        <button>Add todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        <TodoFilter />
       </div>
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         {/* <div className="bg-white text-2xl font-bold p-3 flex justify-center items-center rounded-md">
             <p>There is no task pending</p>
         </div> */}
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          {todos?.data.map((item) => (
+            <TodoCard {...item}  isCompleted={item.isCompleted ?? false} />
+          ))}
+        </div>
       </div>
     </div>
   );
